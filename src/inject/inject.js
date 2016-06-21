a3e71d840e31cadd0eb5ebd830bdaa27f183a972
Arthur L <mardybm@gmail.com>
2016-06-21 18:52:49 +0600

    working prototype

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,25 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === 'complete') {
+      clearInterval(readyStateCheckInterval)
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+      function sortJobs() {
+        var jobs = $('#jobs_list')
+        var jobs_array = jobs.children('.job')
 
-	}
-	}, 10);
-});
+        jobs_array.sort(function(a, b) {
+          var an = Date.parse(replaceAll($(a).find('span.date').html(), months))
+          var bn = Date.parse(replaceAll($(b).find('span.date').html(), months))
+
+          if (an > bn) {
+            return -1
+          }
+          if (an < bn) {
+            return 1
+          }
+          return 0
+        })
+      }
+    }
+  }, 10)
+})
