54bc7907ade8262a5cf8e685f00963373217e119
Arthur L <mardybm@gmail.com>
2016-06-23 19:00:07 +0600

    add russian date handling

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,25 +1,50 @@
+var months = {
+  'января'   : 'jan',
+  'февраля'  : 'feb',
+  'марта'    : 'mar',
+  'апреля'   : 'apr',
+  'мая'      : 'may',
+  'июня'     : 'jun',
+  'июля'     : 'jul',
+  'августа'  : 'aug',
+  'сентября' : 'sep',
+  'октября'  : 'oct',
+  'ноября'   : 'nov',
+  'декабря'  : 'dec'
+}
+
+function replaceAll(str, map) {
+  var re = new RegExp(Object.keys(map).join('|'), 'gi')
+  return str.replace(re, function(matched) {
+    return map[matched]
+  })
+}
+
+function sortJobs() {
+  var jobs = $('#jobs_list')
+  var jobs_array = jobs.children('.job')
+
+  jobs_array.sort(function(a, b) {
+    var an = Date.parse(replaceAll($(a).find('span.date').html(), months))
+    var bn = Date.parse(replaceAll($(b).find('span.date').html(), months))
+
+    if (an > bn) {
+      return -1
+    }
+    if (an < bn) {
+      return 1
+    }
+    return 0
+  })
+
+  jobs_array.detach().appendTo(jobs)
+}
+
 chrome.extension.sendMessage({}, function(response) {
   var readyStateCheckInterval = setInterval(function() {
     if (document.readyState === 'complete') {
       clearInterval(readyStateCheckInterval)
-
-      function sortJobs() {
-        var jobs = $('#jobs_list')
-        var jobs_array = jobs.children('.job')
-
-        jobs_array.sort(function(a, b) {
-          var an = Date.parse(replaceAll($(a).find('span.date').html(), months))
-          var bn = Date.parse(replaceAll($(b).find('span.date').html(), months))
-
-          if (an > bn) {
-            return -1
-          }
-          if (an < bn) {
-            return 1
-          }
-          return 0
-        })
-      }
+      sortJobs()
     }
   }, 10)
 })
