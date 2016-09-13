773da62ee01fe1fcfec066ae7cfdf642bce8cda8
Arthur L <mardybm@gmail.com>
2016-09-13 17:45:14 +0600

    v2.0: add freelansim.ru support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## Changelog
 
+### 2.0
+- Add [freelansim.ru](https://freelansim.ru) support
+- Remade small sized icons (16px & 19px) to be more visible
+
 ### 1.2
 - Refactor with ES6 language spec
 - 100% valid ESLint code
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,21 +1,10 @@
+function curried(fn, ...args) {
+  return (...nArgs) => fn.apply(this, [...args, ...nArgs]);
+}
+
 function replaceAll(str, map) {
   const re = new RegExp(Object.keys(map).join('|'), 'gi');
   return str.replace(re, matched => map[matched]);
-}
-
-function parseRusDate(str) {
-  const num = str.match(/\d+/)[0];
-  const mod = str.match(/(мин|час|дн|ден|мес|год)/)[0];
-  const calendar = {
-    мин: 60,
-    час: 60 * 60,
-    дн: 60 * 60 * 24,
-    ден: 60 * 60 * 24,
-    мес: 60 * 60 * 24 * 30, // I know, I know, sorry
-    год: 60 * 60 * 24 * 365,
-  };
-  const millis = num * calendar[mod] * 1000;
-  return new Date(Date.now() - millis);
 }
 
 function parseRusMonth(str) {
@@ -36,60 +25,62 @@ function parseRusMonth(str) {
   return Date.parse(replaceAll(str, months));
 }
 
-function sortJobs() {
-  const jobsNode = $('#jobs_list');
-  const jobsList = jobsNode.children('.job');
-
-  jobsList.sort((a, b) => {
-    const an = parseRusMonth($(a).find('span.date').html());
-    const bn = parseRusMonth($(b).find('span.date').html());
-
-    if (an > bn) return -1;
-    if (an < bn) return 1;
-    return 0;
-  });
-
-  jobsList.detach().appendTo(jobsNode);
+function parseRusDate(str) {
+  const num = str.match(/\d+/)[0];
+  const mod = str.match(/(мин|час|дн|ден|мес|год)/)[0];
+  const calendar = {
+    мин: 60,
+    час: 60 * 60,
+    дн: 60 * 60 * 24,
+    ден: 60 * 60 * 24,
+    мес: 60 * 60 * 24 * 30, // I know, I know, sorry
+    год: 60 * 60 * 24 * 365,
+  };
+  const millis = num * calendar[mod] * 1000;
+  return new Date(Date.now() - millis);
 }
 
-function sortTasks() {
-  const tasksNode = $('#tasks_list');
-  const tasksList = tasksNode.children('.content-list__item');
+function observeNode(nodeId, performSorting) {
+  let latestURI;
+  $(nodeId).observe('childlist added', (record) => {
+    if (record.addedNodes.length > 0) {
+      if (latestURI !== record.addedNodes[0].baseURI) performSorting();
+      latestURI = record.addedNodes[0].baseURI;
+    }
+  });
+}
 
-  tasksList.sort((a, b) => {
-    const an = parseRusDate($(a).find('span.params__published-at').html());
-    const bn = parseRusDate($(b).find('span.params__published-at').html());
+function sortJobList(nodeId, childClass, dateClass, parser) {
+  const node = $(nodeId);
+  const childList = node.children(childClass);
+
+  childList.sort((a, b) => {
+    const an = parser($(a).find(dateClass).html());
+    const bn = parser($(b).find(dateClass).html());
 
     if (an > bn) return -1;
     if (an < bn) return 1;
     return 0;
   });
 
-  tasksList.detach().appendTo(tasksNode);
+  childList.detach().appendTo(node);
 }
 
 $(document).ready(() => {
+  let nodeId;
+  let performSorting;
+
   if (location.host === 'moikrug.ru') {
-    sortJobs();
-    let latestURI;
-    $('#jobs_list').observe('childlist added', (record) => {
-      if (record.addedNodes.length > 0) {
-        if (latestURI !== record.addedNodes[0].baseURI) sortJobs();
-        latestURI = record.addedNodes[0].baseURI;
-      }
-    });
-    $('#vacancies_search_suggest_form_q')
-      .observe({ attributes: true, attributeFilter: ['class'] }, sortJobs);
+    nodeId = '#jobs_list';
+    performSorting = curried(sortJobList, nodeId, '.job', 'span.date', parseRusMonth);
+    $('#vacancies_search_suggest_form_q').observe({ attributes: true, attributeFilter: ['class'] }, performSorting);
   }
 
   if (location.host === 'freelansim.ru') {
-    sortTasks();
-    let latestURI;
-    $('#tasks_list').observe('childlist added', (record) => {
-      if (record.addedNodes.length > 0) {
-        if (latestURI !== record.addedNodes[0].baseURI) sortTasks();
-        latestURI = record.addedNodes[0].baseURI;
-      }
-    });
+    nodeId = '#tasks_list';
+    performSorting = curried(sortJobList, nodeId, '.content-list__item', 'span.params__published-at', parseRusDate);
   }
+
+  performSorting();
+  observeNode(nodeId, performSorting);
 });
